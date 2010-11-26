70b633c3d12d3208c8c076bc88df73f70fd41e2e
Ari Loytynoja <ari@ebi.ac.uk>
2010-11-26 11:08:18 +0000

    Fixed warnings.

--- main/sequence.cpp
+++ main/sequence.cpp
@@ -57,6 +57,7 @@ void Sequence::create_default_sequence(Fasta_entry &seq_entry)
 
     Site first_site( &edges, Site::start_site, Site::ends_site );
     first_site.set_state( -1 );
+    first_site.set_symbol('0');
     first_site.set_empty_children();
     this->push_back_site(first_site);
 
@@ -89,6 +90,7 @@ void Sequence::create_default_sequence(Fasta_entry &seq_entry)
 
     Site last_site( &edges, Site::stop_site, Site::ends_site );
     last_site.set_state( -1 );
+    last_site.set_symbol('0');
     last_site.set_empty_children();
     this->push_back_site(last_site);
 
@@ -105,6 +107,7 @@ void Sequence::create_fastq_sequence(Fasta_entry &seq_entry, bool no_trimming)
 
     Site first_site( &edges, Site::start_site, Site::ends_site );
     first_site.set_state( -1 );
+    first_site.set_symbol('0');
     first_site.set_empty_children();
     this->push_back_site(first_site);
 
@@ -272,6 +275,7 @@ void Sequence::create_fastq_sequence(Fasta_entry &seq_entry, bool no_trimming)
 
     Site last_site( &edges, Site::stop_site, Site::ends_site );
     last_site.set_state( -1 );
+    last_site.set_symbol('0');
     last_site.set_empty_children();
     this->push_back_site(last_site);
 
@@ -286,6 +290,7 @@ void Sequence::create_graph_sequence(Fasta_entry &seq_entry)
 {
     Site first_site( &edges, Site::start_site, Site::ends_site );
     first_site.set_state( -1 );
+    first_site.set_symbol('0');
     first_site.set_empty_children();
     this->push_back_site(first_site);
 
@@ -310,6 +315,7 @@ void Sequence::create_graph_sequence(Fasta_entry &seq_entry)
 
     Site last_site( &edges, Site::stop_site, Site::ends_site );
     last_site.set_state( -1 );
+    last_site.set_symbol('0');
     last_site.set_empty_children();
     this->push_back_site(last_site);
 
