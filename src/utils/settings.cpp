2e1d83a9d234af3f28613410b5a759963c80e148
Ari Löytynoja <ari.loytynoja@gmail.com>
2012-12-13 09:51:44 +0200

    new option: --test-every-terminal-node

--- src/utils/settings.cpp
+++ src/utils/settings.cpp
@@ -34,8 +34,8 @@ Settings::Settings(){}
 
 int Settings::read_command_line_arguments(int argc, char *argv[])
 {
-    version = 0.47;
-    date = "25 October, 2012";
+    version = 0.48;
+    date = "13 December, 2012";
 
     boost::program_options::options_description minimal("Minimal progressive alignment options",100);
     minimal.add_options()
